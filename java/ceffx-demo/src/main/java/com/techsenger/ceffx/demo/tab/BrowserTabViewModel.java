bc3b5a226ff4ae9496ad4fcb11e7a57ce006e8fa
Pavel Castornii <pavelcastornii@gmail.com>
2026-09-21 00:35:43 +0300

    Adapt to changes in ShellFX related to the close package

--- java/ceffx-demo/src/main/java/com/techsenger/ceffx/demo/tab/BrowserTabViewModel.java
+++ java/ceffx-demo/src/main/java/com/techsenger/ceffx/demo/tab/BrowserTabViewModel.java
@@ -8,9 +8,9 @@ package com.techsenger.ceffx.demo.tab;
 
 import com.techsenger.ceffx.core.CefApp;
 import com.techsenger.ceffx.core.browser.CefBrowserBase;
-import com.techsenger.shellfx.core.CloseCheckResult;
-import com.techsenger.shellfx.core.ClosePreparationResult;
 import com.techsenger.shellfx.core.UiExecutor;
+import com.techsenger.shellfx.core.close.CloseCheckResult;
+import com.techsenger.shellfx.core.close.ClosePreparationResult;
 import com.techsenger.shellfx.core.tab.AbstractTabViewModel;
 import com.techsenger.shellfx.core.tab.TabComposer;
 import com.techsenger.shellfx.material.icon.PlainFontIcon;
